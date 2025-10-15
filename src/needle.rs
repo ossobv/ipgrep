246f32f7d6de1e1b963fa2de19dbec8cbf0b1064
Walter Doekes <walter+github@wjd.nu>
2025-10-15 23:15:11 +0200

    needle: Add some more tests

--- src/needle.rs
+++ src/needle.rs
@@ -27,7 +27,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_needle_valid() {
+    fn test_needle_valid_ip() {
+        let n = Needle::try_from("123.123.123.123").unwrap();
+        assert_eq!(n.src, "123.123.123.123");
+        assert_eq!(n.net, Net::from_str_unchecked("123.123.123.123/32"));
+    }
+
+    #[test]
+    fn test_needle_valid_net() {
+        let n = Needle::try_from("88.99.128.0/17").unwrap();
+        assert_eq!(n.src, "88.99.128.0/17");
+        assert_eq!(n.net, Net::from_str_unchecked("88.99.128.0/17"));
+    }
+
+    #[test]
+    fn test_needle_valid_oldnet() {
         let n = Needle::try_from("192.168.32.0/255.255.224.0").unwrap();
         assert_eq!(n.src, "192.168.32.0/255.255.224.0");
         assert_eq!(n.net, Net::from_str_unchecked("192.168.32.0/19"));
